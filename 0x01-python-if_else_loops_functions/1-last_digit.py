d0d3efe141412e074b3e3b7e40a8b504e89b0188
Koyejo <adinlewakoyejo@yahoo.com>
2023-03-10 17:40:29 -0800

    Task 1 - Complete a source code so that the last digit of a number is printed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 ldg = abs(number) % 10
 if number < 0:
     print(f"Last digit of {number} is -{ldg} and is less than 6 and not 0")
-elif number > 0:
+elif number >= 0:
     if ldg > 5:
         print(f"Last digit of {number} is {ldg} and is greater than 5")
     elif ldg == 0:
