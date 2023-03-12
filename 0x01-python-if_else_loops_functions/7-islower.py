d41314ac303129f8038f0b6a29d816865a2cbf7b
Koyejo <adinlewakoyejo@yahoo.com>
2023-03-11 16:01:01 -0800

    Task 7 - Write a function that checks for lowercase character and returns True or False

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+def islower(c):
+    """Checks if a char is lowercase. Returns: True if so; and False if not"""
+    c = ord(c)
+    if c >= 97 and c <= 122:
+        return True
+    else:
+        return False
