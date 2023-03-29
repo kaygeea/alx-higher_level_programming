42ba48f3efcd754b1ba5e564cffb0a51cd5f0654
Koyejo <adinlewakoyejo@yahoo.com>
2023-03-28 18:53:29 -0700

    Task 9 - Write a function that finds the biggest integer of a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     '''This function finds the biggest integer of a list'''
 
     if len(my_list) == 0:
-        return 0
+        return "None"
 
     tmp_max = my_list[0]
 
