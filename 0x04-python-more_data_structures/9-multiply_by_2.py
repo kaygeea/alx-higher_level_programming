98cbc9615b968897255da583f77961ed8c3e1a33
Koyejo <adinlewakoyejo@yahoo.com>
2023-04-03 16:58:32 -0700

    Task 9 - Write a function that returns a new dictionary with all values multiplied by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    '''This func returns a new dictionary with all values multiplied by 2'''
+    x2dict = {}
+    if a_dictionary != None:
+        for key, value in a_dictionary.items():
+            x2dict[key] = value * 2
+        return x2dict
+    return x2dict
