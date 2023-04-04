c9fc4995057c4c936ab384b836f63e9817d73254
Koyejo <adinlewakoyejo@yahoo.com>
2023-04-03 19:34:10 -0700

    Task 11 - Write a func that returns a list with all values multiplied by a number, without loops

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,6 +2,6 @@
 
 def multiply_list_map(my_list=[], number=0):
     '''Returns a list with all vals mul'd by a number, without loops'''
-
+    mul_list = []
     mul_list = list(map(lambda x: x * number, my_list))
     return mul_list
