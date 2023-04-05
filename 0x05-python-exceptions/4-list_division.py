48fa73c3bb63324b7febe0abaae99c490af331ef
Koyejo <adinlewakoyejo@yahoo.com>
2023-04-04 19:18:45 -0700

    Task 4 - Write a function that divides elems in list 1 by elems in list 2, using exception handling

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    '''Divides elems in list 1 by elems in list 2, using exception handling'''
+    my_list_3 = []
+    for elems in range(0, list_length):
+        try:
+            val = my_list_1[elems] / my_list_2[elems]
+        except TypeError:
+            print("wrong type")
+            val = 0
+        except ZeroDivisionError:
+            print ("division by 0")
+            val = 0
+        except IndexError:
+            print("out of range")
+            val = 0
+        finally:
+            my_list_3.append(val)
+        return (my_list_3)
