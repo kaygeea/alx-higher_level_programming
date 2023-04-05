1398323ea01ffdca4e04dd26c189614d47237117
Koyejo <adinlewakoyejo@yahoo.com>
2023-04-04 19:55:25 -0700

    ADV Task 1 - Write a function that prints an integer using advanced EH

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except Exception:
-        logging.exception('Please check input value')
+    except Exception as err:
+        print(logging.exception)
         return (False)
