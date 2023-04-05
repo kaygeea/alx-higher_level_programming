1319f30ee71d9728167887ad97be80f64f090f93
Koyejo <adinlewakoyejo@yahoo.com>
2023-04-04 18:51:26 -0700

    Task 3 - Write a function that divides 2 integers and prints the result, using EH

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 
 def safe_print_division(a, b):
     '''Divides 2 integers and prints the result, using exception handling.'''
-    
+
     try:
         div = a/b
     except (ZeroDivisionError, TypeError):
