e22d05ed83868020be5fadf4d2e68e4c2c1f6bab
Koyejo <adinlewakoyejo@yahoo.com>
2023-04-16 21:39:10 -0700

    Task 2 - Write a bool func that checks the instanceness of an obj

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,4 +5,7 @@
 def is_same_class(obj, a_class):
     '''This func returns a bool based on if obj is an instance of a_class'''
 
-    return (isinstance(obj, a_class))
+    if isinstance(obj, a_class):
+        return True
+    elif not isinstance(obj, a_class):
+        return False
