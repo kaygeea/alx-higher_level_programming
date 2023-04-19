a993c2bf4ed611f0bb917fe07515893b49067c57
Koyejo <adinlewakoyejo@yahoo.com>
2023-04-18 22:08:18 -0700

    Task 0 - Write a func that reads a text file and prints to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     '''This function reads from a text file and prints to stdout'''
 
     with open(filename, encoding="utf-8") as fl:
-        print(fl.read())
+        print(fl.read(), end='')
