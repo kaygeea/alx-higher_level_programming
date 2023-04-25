62464211fbcbf7f932c531ae6ad6349f6553accd
Koyejo <adinlewakoyejo@yahoo.com>
2023-04-24 20:34:15 -0700

    Task 4 - Write a function that returns a boolean if an obj is an instance of an inherited class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''This module creates a function that performs a derived instance check'''
+
+
+def inherits_from(obj, a_class):
+    '''This function checks if obj() is a derived instance of a_class
+    Args:
+        obj: incoming arg for object to be checked.
+        a_class: incoming arg for class against which obj() is to be checked.
+    Return:
+        True or False, depending on obj()'s type.
+    '''
+
+    if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
