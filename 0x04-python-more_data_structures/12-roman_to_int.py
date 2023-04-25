4e084a59d07f107e171ec9261e0d340abe339ba8
Koyejo <adinlewakoyejo@yahoo.com>
2023-04-25 15:04:44 -0700

    Tech Interview Prep: Task 12 - Write a function that converts a string to roman numeral

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,44 @@
 #!/usr/bin/python3
 '''This module builds a function that converts a Roman numeral to an int'''
-import roman
 
+
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
 
 def roman_to_int(roman_string):
-    '''this function coverts a Roman numeral to an int'''
+    '''This function coverts a Roman numeral string to an int'''
 
-    if roman_string:
-        roman_num = roman.fromRoman(roman_string)
-    return roman_num
+    if not isinstance(roman_string, str):
+        return 0
+
+    if not roman_string:
+        return 0
+
+    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_num.keys())
+
+    num = 0
+    last_rom = 0
+    list_num = [0]
+
+    for char in roman_string:
+        for r_num in list_keys:
+            if r_num == char:
+                if rom_num.get(char) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_num.get(char)]
+                else:
+                    list_num.append(rom_num.get(char))
+
+                last_rom = rom_num.get(char)
+
+    num += to_subtract(list_num)
+
+    return (num)
