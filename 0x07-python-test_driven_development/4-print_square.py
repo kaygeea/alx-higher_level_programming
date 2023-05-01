3ece737aacf079444f9fcc05df3428afb8114eca
Koyejo <adinlewakoyejo@yahoo.com>
2023-05-01 08:30:28 -0700

    Task 3 - Added a function that prints a square with #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module creates a square printing function"""
+
+
+def print_square(size):
+    """This function prints a square with the '#' character.
+    Args:
+        size: incoming argument for the size length of the sqaure
+    Exceptions:
+        TypeError: a. if size is not an integer; and
+                    b. if size is a float and is less that 0.
+        ValueError: if size is less that 0
+    """
+
+    if size == 0:
+        print("")
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    if isinstance(size, int):
+        for height in range(0, size):
+            [print("#", end="") for width in range(size)]
+            print("")
+    elif not isinstance(size, int):
+        raise TypeError("size muts be an integer")
+
+    if isinstance(size, int) and size < 0:
+        raise ValueError("size must be >= 0")
