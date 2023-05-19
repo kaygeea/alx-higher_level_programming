bf0fa1fa567f635f6a5f3e15ef850535f71536e5
Koyejo <adinlewakoyejo@yahoo.com>
2023-05-19 23:53:08 +0100

    Added script that creates a DB, without error

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Create a database named "hbtn_0c_0".
--- If hbtn_0c_0 already exist, the script should not raise an error.
+-- If "hbtn_0c_0" already exist, the script should not raise an error.
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
