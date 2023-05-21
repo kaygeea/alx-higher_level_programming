d6ba7e9af9f57dc08a65c0451ee37c331fbafa23
Koyejo <adinlewakoyejo@yahoo.com>
2023-05-20 19:03:09 -0700

    Added a script that converts a DB to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- Convert hbtn_0c_0 DB to UTF8 encoding.
+-- SPECS:
+--	a. The hbtn_0c_0 DB must be converted.
+--	b. The first_table table must be converted.
+--	c. The name field in first_table must be converted.
+
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
