b922272cc446892964dd5fe440a996c73bae4f16
Koyejo <adinlewakoyejo@yahoo.com>
2023-05-24 14:41:32 -0700

    Added script that creates user based on specs

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,4 +4,4 @@
 --	b. The user_0d_1 password should be set to "user_0d_1_pwd".
 --	c. If the user user_0d_1 already exists, your script should not fail.
 CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO 'user_0d_1';
