cbf77820b6b4ddcf6f37ba1e25381437a2202b21
Koyejo <adinlewakoyejo@yahoo.com>
2023-05-24 15:32:13 -0700

    Added script that creates DB & user based on specs

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,11 @@
 --	b. The user_0d_2 password should be set to "user_0d_2_pwd".
 --	c. If the DB hbtn_0d_2 already exists, your script should not fail.
 --	d. If the user user_0d_2 already exists, your script should not fail.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_02_2.* TO 'user_0d_2'@'localhost';
+CREATE DATABASE
+IF NOT EXISTS hbtn_0d_2;
+CREATE USER
+IF NOT EXISTS 'user_0d_2'@'localhost'
+	IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+ON hbtn_02_2.*
+TO 'user_0d_2'@'localhost';
