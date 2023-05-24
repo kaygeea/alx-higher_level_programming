ba2b2f01f8b077acf805f567ad35e541388a0c65
Koyejo <adinlewakoyejo@yahoo.com>
2023-05-24 14:43:37 -0700

    Added script that creates user based on specs

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,5 +3,5 @@
 --	a. user_0d_1 should have all privileges on your MySQL server.
 --	b. The user_0d_1 password should be set to "user_0d_1_pwd".
 --	c. If the user user_0d_1 already exists, your script should not fail.
-CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
