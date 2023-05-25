fbad58a0d13b658ffa796435ac6433f27f098e53
Koyejo <adinlewakoyejo@yahoo.com>
2023-05-24 23:02:22 -0700

    Added a script with query to join tables

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- List all cities contained in the database - "hbtn_0d_usa".
+-- SPECS:
+--	a. Each record should display according to this column arrangement: cities.id - cities.name - states.name.
+--	b. The results must be sorted in ascending order by cities.id.
+--	c. Only 1 select statement if allowed in the query.
+    SELECT cities.id, cities.name, states.name
+      FROM cities
+INNER JOIN states
+        ON cities.id = states.id;
