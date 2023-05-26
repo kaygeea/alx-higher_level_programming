41e6174f793afd7ade8d47d186b888d598d52b55
Koyejo <adinlewakoyejo@yahoo.com>
2023-05-26 07:02:52 -0700

    Added a script with query to join tables

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,6 +5,6 @@
 --	c. Only 1 select statement if allowed in the query.
     SELECT cities.id, cities.name, states.name
       FROM cities
-INNER JOIN states
-        ON cities.id = states.id
+           INNER JOIN states
+                   ON cities.id = states.id
   ORDER BY cities.id;
