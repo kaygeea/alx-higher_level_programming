f45ed3df84997c090ba0d81e2b574540df6aa803
Koyejo <adinlewakoyejo@yahoo.com>
2023-05-28 03:25:58 -0700

    Added query that performs an inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,8 +3,8 @@
 --	a. Each record should display according to this column arrangement: cities.id - cities.name - states.name.
 --	b. The results must be sorted in ascending order by cities.id.
 --	c. Only 1 select statement if allowed in the query.
-  SELECT cities.id, cities.name, states.name
-    FROM cities
-         INNER JOIN states
-         ON cities.state_id = states.id
-ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name
+  FROM cities
+       INNER JOIN states
+       ON cities.state_id = states.id
+ ORDER BY cities.id;
