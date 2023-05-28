a205260f2923124a80ec77c6dc89fa943e39a9dd
Koyejo <adinlewakoyejo@yahoo.com>
2023-05-28 11:08:47 +0100

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -6,5 +6,5 @@
     SELECT cities.id, cities.name, states.name
       FROM cities
            INNER JOIN states
-                   ON cities.id = states.id
+                   ON cities.state_id = states.id
   ORDER BY cities.id;
