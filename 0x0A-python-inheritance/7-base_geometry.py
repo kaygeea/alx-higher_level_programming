2d78597bd07d1485635920a9cb9543ac41bd15ee
Koyejo <adinlewakoyejo@yahoo.com>
2023-05-31 23:26:09 -0700

    Corrected exception error message

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -25,6 +25,6 @@ class BaseGeometry:
         self.value = value
 
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(self.name))
+            raise TypeError("{} must be an integer".format(self.value))
         if isinstance(value, int) and value <= 0:
-            raise ValueError("{} must be greater than 0".format(self.name))
+            raise ValueError("{} must be greater than 0".format(self.value))
