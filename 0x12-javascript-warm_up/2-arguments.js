18decbe9f0accd74ef39dbfd5434a2f3ec09ede7
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-13 18:53:30 -0700

    Updated comment in code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,15 @@
 #!/usr/bin/node
-// Print message based on argument vector count.
+/*
+ * Print message based on argument vector count.
+ * SPECS:
+ * -> If no arguments are passed to the script, print “No argument”.
+ * -> If only one argument is passed to the script, print “Argument found”.
+ * -> Otherwise, print “Arguments found”.
+ *
+ * CONSTRAINTS:
+ * -> "console.log()" must be used to print all outputs.
+ * -> Use of "var" is not allowed.
+ */
 
 if (process.argv.length === 2) {
   console.log('No arguemnt');
