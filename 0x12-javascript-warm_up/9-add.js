da7b2db14a364d7de416f365cbf45d6031d2d7c8
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-15 09:38:04 -0700

    Added a function that adds 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/* Define function that adds two integers and prints the sum.
+ * SPECS:
+ * -> The first argument is the first integer.
+ * -> The second argument is the second integer.
+ * -> Define a function with this prototype: "function add(a, b)"
+ *
+ * CONSTRAINTS:
+ * -> "console.log()" must be used to print all outputs.
+ * -> Use of "var" is not allowed.
+ */
+
+function add (a, b) {
+  return a + b;
+}
+
+console.log(add(parseInt(process.argv[2], 10), parseInt(process.argv[3], 10)));
