f2d2fb1dfdf6f57a8c18764be022f026d614cbff
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-15 12:15:48 -0700

    Updated script to mutate property of const object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* Update given script to replace 12 as "value" with 98.
+ *
+ * CONSTRAINTS:
+ * -> Use of "var" is not allowed.
+ */
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89; // Properties of objects assigned to constants are not protected. This they can be changed.
+console.log(myObject);
