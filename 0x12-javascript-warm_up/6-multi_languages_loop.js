73f7c90ce0cce6b02be698b1a51ede76d8a4a683
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-15 07:27:55 -0700

    Added code that creates, loops though & print elements of array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* Create then loop through an array of strings, printing each element.
+ * CONSTRAINTS:
+ * -> "console.log()" must be used to print all outputs.
+ * -> Use of "var" is not allowed.
+ * -> Use of if/else statement is not allowed.
+ * -> "console.log()" can only be used once in the code.
+ * -> Use of a loop (while, for, etc) is mandatory.
+ */
+
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const output of strings) {
+  console.log(output);
+}
