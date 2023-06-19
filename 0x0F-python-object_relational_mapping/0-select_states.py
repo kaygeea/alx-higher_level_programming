ca036685d0f0d6118ae7e96eea9fe9d4d3e27567
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-19 11:13:22 -0700

    Added script that lists all states from a DB

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""List all states from the database 'hbtn_0e_0_usa'
+SPECS:
+    a. The script takes 3 args: mysql username, mysql password & database name
+    b. Connect to a MySQL server running on localhost at port 3306
+    c. Results must be sorted in ascending order by states.id
+
+CONSTRAINTS:
+    a. The module 'MySQLdb' must be used.
+    b. The code should not be executed when imported.
+"""
+from mysql.connector import connect, Error
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    try:
+        with MySQLdb.connect(
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
+                ) as connection:
+                list_states_query = "SELECT * FROM `states`;"
+
+                with connection.cursor() as cursor:
+                    cursor.execute(list_states_query)
+                    for id, name in cursor.fetchall():
+                        print("({}, \'{}\')".format(id, name))
+    except Error as err_msg:
+        print(err_msg)
