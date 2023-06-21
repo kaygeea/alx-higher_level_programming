448e5832675edf70e016678688b2b0ce14170a72
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-21 07:52:54 -0700

    Added a script that lists cities of states passed as args (safe from SQLI)

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module takes in the name of a state as an argument and
+lists all cities of that state, all from the database hbtn_0e_4_usa.
+"""
+from mysql.connector import connect, Error
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    try:
+        with MySQLdb.connect(
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
+                ) as connection:
+            list_cities_by_state_query = """
+            SELECT cities.name
+              FROM cities
+                   INNER JOIN states
+                   ON cities.state_id = states.id
+                   WHERE states.name = %s
+             ORDER BY cities.id;
+            """
+
+            with connection.cursor() as cursor:
+                cursor.execute(list_cities_by_state_query, (sys.argv[4],))
+                for city in cursor.fetchall():
+                    print(city)
+    except Error as err_msg:
+        print(err_msg)
