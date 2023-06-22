0500b01c1e8ddc1b7c605900da9804709d93ba17
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-22 05:37:13 -0700

    Made spelling correction to class documentation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 class State(Base):
     """Represent and link to a MySQL table - "state",
     with the table columns represented as class attributes:
-        a. id (PK) - A column of an auto-gen'd, unique in, and not null.
+        a. id (PK) - A column of an auto-gen'd, unique int and not null.
         b. name - A column of a str of max 128 chars and not null.
     """
     __tablename__ = 'states'
