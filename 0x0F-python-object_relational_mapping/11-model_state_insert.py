61ca336d1ac861b72516da345ce914d0756f85a4
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-23 07:53:04 -0700

    Added script that adds new State obj to DB

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module adds a new State obj: 'Louisiana', to the hbtn_0e_6_usa DB"""
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+url = URL.create('mysql+mysqldb', username=argv[1], password=argv[2],
+                 host='localhost', port=3306, database=argv[3])
+v10_engine = create_engine(url)
+Session = sessionmaker(v10_engine)
+
+with Session.begin() as session:  # .begin() runs cntxt manager & auto-commits
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+
+    new_state_obj = session.query(State).filter(State.name == 'Louisiana').\
+            first()
+    print('{}'.format(new_state_obj.id))
+
+    # for updated_states in session.query(State).order_by(State.id):
+        # print("{}: {}".format(updated_states.id, updated_states.name))
