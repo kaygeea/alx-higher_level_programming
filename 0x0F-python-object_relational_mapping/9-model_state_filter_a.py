e0132499c9be4f77fe400703f8a29edd9c694b95
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-22 23:31:31 -0700

    Added script to print all State objs name with "a" from a DB

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Lists all State objs from the database "hbtn_0e_6_usa", that contain
+the letter "a", using SQLAlchemy.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+
+url = URL.create('mysql+mysqldb', username=argv[1], password=argv[2],
+                 host='localhost', port=3306, database=argv[3])
+v6_engine = create_engine(url)
+
+with Session(v6_engine) as session:
+    for state_obj in session.query(State).filter(State.name.like('%a%')).\
+            order_by(State.id):
+                print("{}: {}".format(state_obj.id, state_obj.name))
