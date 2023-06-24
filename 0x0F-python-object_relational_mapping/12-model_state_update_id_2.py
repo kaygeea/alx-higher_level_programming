501d4826bd36aa47e8e0a88c8afd5a9475b39fb8
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-24 03:17:53 -0700

    Added code to change name of State object in DB

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module changes the name of a State object from the database,
+'hbtn_0e_6_usa', based on a given condition.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = URL.create('mysql+mysqldb', username=argv[1], password=argv[2],
+                     host='localhost', port=3306, database=argv[3])
+    v12_engine = create_engine(url)
+    Session = sessionmaker(v12_engine)
+
+    with Session.begin() as session:
+        new_name = session.query(State).filter_by(id=2).first()
+        new_name = 'New Mexico'
