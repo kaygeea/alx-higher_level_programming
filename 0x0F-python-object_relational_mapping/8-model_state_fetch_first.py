4be2ebb0922f081dec83a9a3e50e8b78551b9730
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-24 00:26:34 -0700

    Made code non-executable upon import

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,12 +9,13 @@ from model_state import Base, State
 from sys import argv
 
 
-url = URL.create('mysql+mysqldb', username=argv[1], password=argv[2],
-                 host='localhost', port=3306, database=argv[3])
-v6_engine = create_engine(url)
+if __name__ == "__main__":
+    url = URL.create('mysql+mysqldb', username=argv[1], password=argv[2],
+                     host='localhost', port=3306, database=argv[3])
+    v6_engine = create_engine(url)
 
-with Session(v6_engine) as session:
-    first_state_obj = session.query(State).first()
+    with Session(v6_engine) as session:
+        first_state_obj = session.query(State).first()
 
     if first_state_obj is None:
         print("Nothing")
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -10,11 +10,12 @@ from model_state import Base, State
 from sys import argv
 
 
-url = URL.create('mysql+mysqldb', username=argv[1], password=argv[2],
-                 host='localhost', port=3306, database=argv[3])
-v6_engine = create_engine(url)
+if __name__ == "__main__":
+    url = URL.create('mysql+mysqldb', username=argv[1], password=argv[2],
+                     host='localhost', port=3306, database=argv[3])
+    v6_engine = create_engine(url)
 
-with Session(v6_engine) as session:
-    for state_obj in session.query(State).filter(State.name.like('%a%')).\
-            order_by(State.id):
-                print("{}: {}".format(state_obj.id, state_obj.name))
+    with Session(v6_engine) as session:
+        for state_obj in session.query(State).filter(State.name.like('%a%')).\
+                order_by(State.id):
+            print("{}: {}".format(state_obj.id, state_obj.name))
