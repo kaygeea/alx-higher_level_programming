27baa4f671e06a04b9a5b2dd23b4adde7351ae8d
Koyejo <adinlewakoyejo@yahoo.com>
2023-06-24 00:18:13 -0700

    Made code non-executable upon import

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module prints State objs from the database "hbtn_0e_6_usa", whose name
+This module prints State objs from the database 'hbtn_0e_6_usa', whose name
 matches the name passed as command-line argument, using SQLAlchemy.
 """
 from sqlalchemy import create_engine
@@ -10,15 +10,16 @@ from model_state import Base, State
 from sys import argv
 
 
-url = URL.create('mysql+mysqldb', username=argv[1], password=argv[2],
-                 host='localhost', port=3306, database=argv[3])
-v8_engine = create_engine(url)
+if __name__ == "__main__":
+    url = URL.create('mysql+mysqldb', username=argv[1], password=argv[2],
+                     host='localhost', port=3306, database=argv[3])
+    v8_engine = create_engine(url)
 
-with Session(v8_engine) as session:
-    cl_name_state_obj = session.query(State).filter(State.name == argv[4]).\
-            first()
+    with Session(v8_engine) as session:
+        cl_name_state_obj = session.query(State).\
+            filter(State.name == argv[4]).first()
 
-    if not cl_name_state_obj:
-        print("Not found")
-    else:
-        print("{}".format(cl_name_state_obj.id))
+        if cl_name_state_obj is None:
+            print("Not found")
+        else:
+            print("{}".format(cl_name_state_obj.id))
