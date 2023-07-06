db7c3998ff9cb176ce369b82b8c0c99c731557c6
Koyejo <adinlewakoyejo@yahoo.com>
2023-07-06 12:17:10 -0700

    Added script to fetch given URL and output body as specified

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""This module builds a script that fetches a given URL."""
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    alx_url = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(alx_url) as response_obj:
+        body = response_obj.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
