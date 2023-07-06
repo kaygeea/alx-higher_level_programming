1c667a6051f0a85d19dcc98e6954d659841a36ba
Koyejo <adinlewakoyejo@yahoo.com>
2023-07-06 15:41:01 -0700

    Added script to POST email to URL and display resp body

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Build a script to send POST request to a URL of an email to a URL,
+and display the body of the response (decoded in utf-8)
+
+USAGE: ./2-post_email.py <URL> <email>
+"""
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from sys import argv
+
+
+if __name__ == "__main__":
+    data_to_post = {'email' : argv[2]}
+    encoded_data = urlencode(data_to_post).encode('utf-8')
+    post_req = Request(argv[1], data=encoded_data)
+
+    with urlopen(post_req) as response:
+        decoded_data = response.read().decode('utf-8')
+        print(decoded_data[:-2])  # Sliced off 2 xtra chars printed with output
