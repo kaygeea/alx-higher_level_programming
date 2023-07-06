d880d46633fa2eaaab4d790e35dbdfa125991c24
Koyejo <adinlewakoyejo@yahoo.com>
2023-07-06 13:05:15 -0700

    Added script to GET URL and display specified header var

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Build a script to GET a URL and display a specified header variable"""
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    arg_url = argv[1]
+    with urllib.request.urlopen(arg_url) as response_obj:
+        header = response_obj.info()
+        print(header.get('X-Request-Id'))
