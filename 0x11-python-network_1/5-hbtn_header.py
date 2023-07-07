ef59c959a07e85395c397fbdc0625ce10a109e15
Koyejo <adinlewakoyejo@yahoo.com>
2023-07-06 17:16:12 -0700

    Added script to GET URL and display specified header var with Reqs

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Contain a script that GETs a URL and display a specified header variable"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    resp_obj = requests.get(argv[1])
+    print(resp_obj.headers['x-request-id'])  # case-insensitivity
