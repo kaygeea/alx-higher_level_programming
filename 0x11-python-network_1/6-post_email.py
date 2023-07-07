afc6be86acc7ea1df2ff589c6d51153a348350c1
Koyejo <adinlewakoyejo@yahoo.com>
2023-07-06 17:30:15 -0700

    Added script to POST email to URL and display resp body

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+   Contains a script that sends a POST request to a URL of an email to a URL,
+   and displays the body of the response.
+
+   USAGE: ./6-post_email.py <URL> <email>
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    post_req = requests.post(argv[1], data={'email': argv[2]})
+    print(post_req.text)
