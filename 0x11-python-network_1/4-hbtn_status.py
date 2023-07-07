328d9fbc01a392828e771e2f3a70be3f0a440d2a
Koyejo <adinlewakoyejo@yahoo.com>
2023-07-06 17:03:01 -0700

    Added script to fetch URL with Requests pkg

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Build a script that uses the Requests package to fetch a given URL"""
+from requests import get
+
+
+if __name__ == "__main__":
+    req_url = get('https://alx-intranet.hbtn.io/status')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(req_url.text)))
+    print("\t- content: {}".format(req_url.text))
