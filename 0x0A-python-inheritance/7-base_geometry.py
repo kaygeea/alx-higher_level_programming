d8d07df918a21528a9088cd7b0d4705d9c9e0a5c
Koyejo <adinlewakoyejo@yahoo.com>
2023-08-07 05:29:59 -0700

    Made syntax and lexical corrections

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,17 +3,17 @@
 
 
 class BaseGeometry:
-    '''This class creates a class BaseGeometry'''
+    '''Create a class BaseGeometry'''
 
     pass
 
     def area(self):
-        '''This non-implemented method raises an Exception'''
+        '''Raises an Exception'''
 
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        '''This method validates incoming arg - value
+        '''Validate incoming arg - value
         Args:
             name: incoming arg for name of item to which value is assigned
             value: incoming arg for value of name
@@ -22,9 +22,10 @@ class BaseGeometry:
             ValueError: if value is less than 0
         '''
         self.name = name
-        self.value = value
 
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(self.value))
-        if isinstance(value, int) and value <= 0:
-            raise ValueError("{} must be greater than 0".format(self.value))
+            raise TypeError("{} must be an integer".format(self.name))
+        elif isinstance(value, int) and value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
+        else:
+            self.value = value
